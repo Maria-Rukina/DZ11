e7648e3d3ec3fa4bfb4ac56f4215ff850930dfdc
user <user@WIN-EDG70QA3MEF>
2025-11-12 20:36:06 +0300

    Добавьте файлы проекта.

---
+++ DZ11/FileName.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_DEPRECATE 
+#include <stdio.h> 
+#include <locale.h> 
+#define _USE_MATH_DEFINES 
+#include <math.h>
+#define N 5
+ 
+void main() 
+{ 
+	setlocale(LC_ALL, "Rus"); 
+	 
+	puts("¬ариант 23");
+
+	int A[N], temp, s = 0, p = 0;
+	printf("¬ведите 5 челых чисел");
+	for (int i = 0; i < N; i++)
+	{ 
+		printf("\na=[%d]", i + 1); 
+		scanf("%f", &temp); 
+		A[i] = temp; 
+	} 
+	 
+	for (int i = 0; i < N; i++)
+	{ 
+		if (A[i] / 2 == 0)  
+		{ 
+			s++; 
+		}  
+		if (A[i] > 0) 
+		{ 
+			p++; 
+		} 
+	} 
+	 
+	double o = (double)s / p;
+	printf("ќтношение кол-во чЄтных чисел массива к кол-во положительных чисел массива %lf", o); 
+	 
+	return 0; 
+} 
